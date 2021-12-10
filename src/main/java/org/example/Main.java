497ae78f6c7f813ec958f25640cc9e01220cabf9
Edgar_Chacon_1548 <edgar.chacon@utec.edu.pe>
2021-12-09 22:01:15 -0500

    avance

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import java.util.logging.Logger;
-
-public class Main {
-    static final Logger logger = Logger.getLogger(Main.class.getName());
-    public static void main(String[] args) {
-        Main.logger.info("My Message");
-    }
-}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Student {
+    private String _Nombre;
+    Student(String nombre) {
+        _Nombre = nombre;
+    }
+}
